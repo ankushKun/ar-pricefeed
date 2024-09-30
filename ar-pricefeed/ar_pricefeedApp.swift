31285ded7398528fbfd6c8f20006b5352ee16471
ankushKun <ankush4singh@gmail.com>
2024-09-30 10:25:16 +0530

    Initial Commit

---
+++ ar-pricefeed/ar_pricefeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ar_pricefeedApp.swift
+//  ar-pricefeed
+//
+//  Created by Ankush Singh on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ar_pricefeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ar-pricefeedTests/ar_pricefeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ar_pricefeedTests.swift
+//  ar-pricefeedTests
+//
+//  Created by Ankush Singh on 30/09/24.
+//
+
+import Testing
+@testable import ar_pricefeed
+
+struct ar_pricefeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
